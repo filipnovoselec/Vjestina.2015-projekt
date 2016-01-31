3f957b3c13c0ecc658e5b1f2c10a1cec2f2b31f3
Filip Novoselec <filip.novoselec@gmail.com>
2016-01-31 15:32:38 +0100

    no message

--- projekt_v3/projekt_v3/GradeModel.Designer.cs
+++ projekt_v3/projekt_v3/GradeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\filip\Documents\Vještina c#\Projekt 3\projekt_v3\projekt_v3\GradeModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\filip\Documents\Vještina c#\Projekt\projekt_v3\projekt_v3\GradeModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
